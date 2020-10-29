5e53bccfbd99301df6e6e5adc028c1fd4fac9366
mikiwang820 <61928785+mikiwang820@users.noreply.github.com>
2020-10-29 10:36:55 +0800

    Update auto_remove_postfile.py

--- auto_remove_postfile.py
+++ auto_remove_postfile.py
@@ -12,7 +12,14 @@ import os
 
 
 #remove
+#set parameters from here
 route = '/Volumes/dataBackup/HCC/infer_0831_1650/train'
+
+text = 'post_v'
+
+text_num = 5
+
+#########################################################################################
 
 mask_list = []
 
@@ -20,7 +27,7 @@ for root, dirs, files in os.walk(route):
 
     for file in files:
         
-        if file[:6] == 'post_v':
+        if file[:text_num + 1] == text:
             
             filename = os.path.join(root, file)
             
@@ -31,7 +38,14 @@ for root, dirs, files in os.walk(route):
 
 
 #move
+#set parameters from here
 route = '/Users/wangmeiqi/code_and_data/HCC/output/infer_0831_1650/test/Regi_Reso_PC'
+
+text = 'post_'
+
+text_num = 4
+
+#########################################################################################
 
 mask_list = []
 
@@ -39,7 +53,7 @@ for root, dirs, files in os.walk(route):
 
     for file in files:
         
-        if file[:5] == 'post_':
+        if file[:text_num + 1] == text:
             
             filename = os.path.join(root, file)
             
